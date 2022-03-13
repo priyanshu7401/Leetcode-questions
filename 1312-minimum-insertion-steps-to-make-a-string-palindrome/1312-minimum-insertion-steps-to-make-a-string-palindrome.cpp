24cfced91f7be7d585cdaad0d744c6ba2a5acd8b
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-03-13 16:24:54 +0530

    Time: 49 ms (55.87%), Space: 27 MB (42.31%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
+    vector<vector<int>>memo;
     int dp(string &s,int i,int j)
     {
-        if(i>=j)							//Base case.
+        if(i>=j)
             return 0;
-        if(memo[i][j]!=-1)					//Check if we have already calculated the value for the pair `i` and `j`.
+        if(memo[i][j]!=-1)
             return memo[i][j];
-        return memo[i][j]=s[i]==s[j]?dp(s,i+1,j-1):1+min(dp(s,i+1,j),dp(s,i,j-1));		//Recursion as mentioned above.
+        return memo[i][j]=(s[i]==s[j])?dp(s,i+1,j-1):1+min(dp(s,i+1,j),dp(s,i,j-1));
     }
     int minInsertions(string s) 
     {
-        memo.resize(s.length(),vector<int>(s.length(),-1));
-        return dp(s,0,s.length()-1);
+        int n=size(s);
+        memo.resize(n,vector<int>(n,-1));
+        return dp(s,0,n-1);
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> memo;
+//     int dp(string &s,int i,int j)
+//     {
+//         if(i>=j)							//Base case.
+//             return 0;
+//         if(memo[i][j]!=-1)					//Check if we have already calculated the value for the pair `i` and `j`.
+//             return memo[i][j];
+//         return memo[i][j]=s[i]==s[j]?dp(s,i+1,j-1):1+min(dp(s,i+1,j),dp(s,i,j-1));		//Recursion as mentioned above.
+//     }
+//     int minInsertions(string s) 
+//     {
+//         memo.resize(s.length(),vector<int>(s.length(),-1));
+//         return dp(s,0,s.length()-1);
+//     }
+// };
