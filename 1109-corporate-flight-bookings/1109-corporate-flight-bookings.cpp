af3a97c6469fae573968f0262dab0f5c132eb991
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-03-13 16:00:18 +0530

    Time: 267 ms (64.24%), Space: 67.7 MB (71.65%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -5,9 +5,9 @@ public:
     vector<int>prefix(n,0);
     for(int i=0;i<size(bookings);i++)
     {
-        prefix[bookings[i][0]-1]+=bookings[i][2];
+        prefix[bookings[i][0]-1]+=bookings[i][2];//if(starting add it into the path rememebr (prefix is zero index and given data is 1 indexed))
         if(bookings[i][1]<n)
-            prefix[bookings[i][1]]+=-1*bookings[i][2];    
+            prefix[bookings[i][1]]+=-1*bookings[i][2];//if that booking ends simply do -of that value so that it is removed from the current totally booked values.
     }
     int sum=0;
     for(int i=1;i<n;i++)
