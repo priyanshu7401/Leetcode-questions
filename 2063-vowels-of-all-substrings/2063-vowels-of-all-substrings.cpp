24796b3ea0865c9b671d430a9af6a6b4a5e0aa81
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-03-15 01:46:53 +0530

    Time: 52 ms (21.87%), Space: 11 MB (89.00%) - LeetHub

---
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -0,0 +1,18 @@
+#define ll long long
+class Solution {
+public:
+    long long countVowels(string word) 
+    {
+    ll last=0,prefix=0,n=size(word);
+    string vowel="aeiou";
+    for(int i=0;i<n;i++)
+    {
+        if(vowel.find(word[i])!=string::npos)
+        {
+            last=(last+(i+1));
+        }
+        prefix=(prefix+last);
+    }
+        return prefix;
+    }
+};
