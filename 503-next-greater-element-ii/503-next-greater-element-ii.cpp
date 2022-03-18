a49f26dd694c1c9dc21c84d46fafbb2be9b3ecf2
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-03-18 15:36:10 +0530

    Time: 40 ms (73.45%), Space: 23.9 MB (60.37%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -5,15 +5,14 @@ public:
     int n=size(nums);
     stack<int>st;
     vector<int>ans(n,-1);
-    for(int i=0;i<n;i++)nums.push_back(nums[i]);
     for(int i=0;i<2*n;i++)
     {
-        while(!st.empty()&&nums[st.top()]<nums[i])
+        while(!st.empty()&&nums[st.top()]<nums[i%n])
         {
-            ans[st.top()%n]=nums[i];
+            ans[st.top()%n]=nums[i%n];
             st.pop();
         }
-        st.push(i);
+        st.push(i%n);
     }
         return ans;
     }
