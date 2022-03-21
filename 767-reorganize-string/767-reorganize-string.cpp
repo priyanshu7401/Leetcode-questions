9e32d90e5427ec262c23580232b5690582bf1ec4
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-03-21 11:21:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (88.92%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -3,6 +3,7 @@ public:
     string reorganizeString(string s) 
     {
         unordered_map<char,int>map;
+        int n=size(s);
         for(int i=0;i<size(s);i++)map[s[i]]++;
         vector<pair<char,int>>vec;
         for(auto [i,j]:map)
@@ -16,23 +17,16 @@ public:
         int i=0,j=0;
         for(;i<size(vec)&&j<s.size();)
         {
+            
             s[j]=vec[i].first;
             vec[i].second--;
-            j+=2;
+            if(j%2&&s[j-1]==s[j])
+                return "";
+            j=(j+2)%n;
+            if(j==0)j=1;
             if(vec[i].second==0)
                 i++;
         }
-        j=1;
-        for(;i<size(vec)&&j<s.size();)
-        {
-            s[j]=vec[i].first;
-            if(s[j-1]==s[j])
-                return "";
-            vec[i].second--;
-            j+=2;
-            if(vec[i].second==0)
-                i++;
-        }        
         return s;
     }
 };
