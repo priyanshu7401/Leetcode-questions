039b55dd59da20cfbb639bae6772ab84361fc604
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-06-09 23:46:48 +0530

    Time: 199 ms (84.94%), Space: 89.6 MB (54.65%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) 
+    {
+        int i=k,j=k,ans=nums[k],n=size(nums),mini=nums[k];
+        while(i>=1||j+1<n)
+        {
+            int num_init=(i-1>=0)?nums[i-1]:INT_MIN;
+            int num_last=(j+1)<n?nums[j+1]:INT_MIN;
+            if(num_init>num_last)
+            {
+                mini=min(num_init,mini);
+                i--;
+                ans=max(ans,mini*(j-i+1));
+            }
+            else
+            {
+                mini=min(num_last,mini);
+                j++;
+                ans=max(ans,mini*(j-i+1));             
+            }
+        }
+        return ans;
+    }
+};
