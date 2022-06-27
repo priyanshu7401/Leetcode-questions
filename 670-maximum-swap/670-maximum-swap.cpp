3974651ec18b98975e91539d39e135f24a3441ee
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-06-27 14:17:10 +0530

    Time: 4 ms (22.79%), Space: 6 MB (60.51%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximumSwap(int num) 
+    {
+        
+        string s=to_string(num);
+        
+        int n=size(s);
+        vector<int>store(10,0);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            store[s[i]-'0']=max(store[s[i]-'0'],i);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=9;j>=s[i]+1-'0';j--)
+            {
+                if(store[j]>i)
+                {
+                    swap(s[store[j]],s[i]);
+                    // cout<<j<<" "<<s<<endl;
+                    i=n;
+                    break;
+                }
+            }
+        }
+        
+        return stoi(s);
+    }
+
+};
