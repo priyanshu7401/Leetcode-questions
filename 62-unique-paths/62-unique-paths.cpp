f8275a695fe7b713710788c02b426079cc207fa5
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-08-01 23:54:39 +0530

    Time: 7 ms (7.14%), Space: 6.2 MB (59.42%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) 
+    {
+     vector<int>dp(m,1);
+        dp[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+              dp[j]+=dp[j-1];
+            }
+        }
+        return dp[m-1];
+    }
+};
