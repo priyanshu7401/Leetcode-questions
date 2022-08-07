bd564145c3a9a580cd1785e55872b0e9d1ebf523
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-08-07 09:46:43 +0530

    Time: 247 ms (55.56%), Space: 90.4 MB (11.11%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,24 +2,21 @@ int dp[100005];
 class Solution {
 public:
     bool check(vector<int> &a, int i) {
-        if(i == a.size()) {
+        if(i >= a.size()) {
             return 1;
         }
         if(dp[i] != -1) {
             return dp[i];
         }
-        bool a1 = (i<=a.size()-2 ? (a[i]==a[i+1] and check(a, i+2)) : 0);
-        bool a2 = (i<=a.size()-3 ? (a[i]==a[i+1] and a[i+1]==a[i+2] and check(a, i+3)) : 0);
-        bool a3 = (i<=a.size()-3 ? (a[i]+1==a[i+1] and a[i+1]+1==a[i+2] and check(a, i+3)) : 0);
+        bool a1 = ((i<=a.size()-2) ? (a[i]==a[i+1] and check(a, i+2)) : 0);
+        bool a2 = ((i+3<=a.size()) ? ((a[i]==a[i+1]) and (a[i+1]==a[i+2]) and check(a, i+3)) : 0);
+        bool a3 = ((i+3<=a.size()) ? ((a[i]+1==a[i+1]) and (a[i+1]+1==a[i+2]) and check(a, i+3)) : 0);
         return dp[i] = (a1 or a2 or a3 ? 1 : 0);
     }
     bool validPartition(vector<int>& a) {
         // The subarray consists of exactly 2 equal elements. For example, the subarray [2,2] is good.
         // The subarray consists of exactly 3 equal elements. For example, the subarray [4,4,4] is good.
         // The subarray consists of exactly 3 consecutive increasing elements, that is, the difference between adjacent elements is 1. For example, the subarray [3,4,5] is good, but the subarray [1,3,5] is not.
-        if(a.size() == 2) {
-            return a.front() == a.back();
-        }
         memset(dp, -1, sizeof(dp));
         return check(a, 0);
     }
