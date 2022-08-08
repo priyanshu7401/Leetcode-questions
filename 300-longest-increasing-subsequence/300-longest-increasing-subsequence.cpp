a40cb73ce08580dfe0c9ad5846822a0e72df2be1
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-08-09 02:59:42 +0530

    Time: 7 ms (97.92%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>>memo;
-    int dp(const vector<int> &A,int i,int j,vector<vector<int>>&memo)
-    {
-        if(j-1==size(A))
-            return 0;
-
-        if(memo[i][j]!=-1)
-            return memo[i][j];
-
-        if(i==0||A[j-1]>A[i-1])
-        {
-            memo[i][j]=1+dp(A,j,j+1,memo);
-        }
-
-        return memo[i][j]=max(memo[i][j],dp(A,i,j+1,memo));
-
+    int lengthOfLIS(vector<int>& nums) {
+    vector<int> res;
+    for(int i=0; i<nums.size(); i++) {
+        auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+        if(it==res.end()) res.push_back(nums[i]);
+        else *it = nums[i];
     }
-    int lengthOfLIS(vector<int>& A) 
-    {
-        int n=size(A);
-        vector<vector<int>>memo(n+1,vector<int>(n+1,-1));
-        return dp(A,0,1,memo);   
-    }
+    return res.size();
+}
 };
