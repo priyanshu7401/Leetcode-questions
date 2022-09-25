239b6db2a2d2253b2af5696980a2bf519d4c1182
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-09-25 12:54:47 +0530

    Time: 1004 ms (30.00%), Space: 130.8 MB (30.00%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     int parent[100001]={0};
     int rank[100001]={0};
+    
     int find(int parent[],int i)
     {
         if(parent[i]==i)
             return i;
         return parent[i]=find(parent,parent[i]);//path compression
     }
+    
     int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) 
     {
         int n=vals.size(),ans=n;
         vector<vector<int>>component(n,vector<int>(2,-1));
-        auto mycmp=[&](vector<int>&a,vector<int>&b)
-        {
+        auto mycmp=[&](vector<int>&a,vector<int>&b){
             return max(vals[a[0]],vals[a[1]])< max(vals[b[0]],vals[b[1]]);
         };
         
         sort(begin(edges),end(edges),mycmp);
         
-        for(int i=0;i<n;i++)
-        {
-            parent[i]=i;
-            rank[i]=0;
-        }
+        for(int i=0;i<n;i++)parent[i]=i, rank[i]=0;
+
         int maxi=-1;
         for(auto vec:edges)
         {
@@ -34,7 +32,6 @@ public:
                 
                 if(component[par0][1]<maxi)component[par0][0]=(vals[par0]==maxi),component[par0][1]=maxi;
                 if(component[par1][1]<maxi)component[par1][0]=(vals[par1]==maxi),component[par1][1]=maxi;
-                // cout<<vec[0]<<" "<<vec[1]<<" "<<par0<<" "<<par1<<" "<<component[par0][0]<<" "<<component[par1][0]<<" "<<maxi<<endl;
                 ans+=component[par0][0]*component[par1][0];
                 component[par1][0]=component[par0][0]+component[par1][0];
                 
