863e7b126c5110d3114314c5eff4819d2aecc54d
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-09-26 08:32:09 +0530

    Time: 10 ms (57.64%), Space: 11.3 MB (88.77%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+ 	vector<int>parent;
+ 	int find(int x) {
+ 		return parent[x] == x ? x : (parent[x]=find(parent[x]));
+ 	}
+ public:
+ 	 bool equationsPossible(vector<string>& equations) {
+
+ 		int n = equations.size();
+ 		parent.resize(26, 0);
+ 		for (int i = 0; i <26; i++)
+ 			parent[i] = i;
+         
+ 		for (int i = 0; i < n; i++) 
+        {
+            if(equations[i][1]!='=')continue;
+ 			int x = find(equations[i][0]-'a');
+ 			int y = find(equations[i][3]-'a');
+ 			if (x != y)
+ 				parent[y] = x;
+ 		}
+ 		for (int i = 0; i < n; i++) 
+        {
+            if(equations[i][1]!='!')continue;	
+            int x = find(equations[i][0]-'a');
+ 			int y = find(equations[i][3]-'a');
+ 			if (x == y)
+ 				return false;
+ 		}         
+
+ 		return true;
+ 	}
+ };
