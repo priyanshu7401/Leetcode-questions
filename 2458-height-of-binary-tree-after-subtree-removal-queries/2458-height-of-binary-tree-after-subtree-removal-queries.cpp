1f0e0837e1ebb663dd7aa1b7d24498b40ef5db46
PRIYANSHU CHAUDHARY <57743477+priyanshu7401@users.noreply.github.com>
2022-10-30 11:03:00 +0530

    Time: 260 ms (100.00%), Space: 123.4 MB (77.78%) - LeetHub

--- 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -1,19 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
 
-    int dp[100001]={0};
-    int maxi1=0,maxi2=0;
+    vector<int>dp;
+    int maxi1=0,maxi2=0,nodes=0;
+    void count(TreeNode *root)
+    {
+        if(root==NULL)
+            return;
+        count(root->left);
+        count(root->right);
+        nodes+=1;
+    }
     void dfs(TreeNode *root,int height=0)
     {
         if(root==NULL)
@@ -21,7 +18,8 @@ public:
         dp[root->val]=max(maxi2,dp[root->val]);  
         maxi2=max(maxi2,height);      
         dfs(root->left,height+1);
-        dfs(root->right,height+1);          
+        dfs(root->right,height+1);  
+        nodes+=1;
     }
     
     void reversedfs(TreeNode *root,int height=0)
@@ -39,6 +37,8 @@ public:
     vector<int> treeQueries(TreeNode* root, vector<int>& queries) 
     {
         vector<int>ans;
+        count(root);
+        dp.resize(nodes+1,0);
         dfs(root,0);
         reversedfs(root,0);
           
